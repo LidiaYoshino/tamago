1614ac53715278c741eda4748a215e8e1c964e6c
lidiayoshino <lidiayoshino@gmail.com>
2019-10-28 01:03:06 -0200

    refactor: change main page to styled components

---
+++ src/containers/titleScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Title from '../components/Title';
+import Header from '../components/Header';
+
+const TitleScreen = () => {
+  return (
+    <Header>
+      <Title>
+        Tamago.
+      </Title>
+    </Header>
+  );
+}
+
+export default TitleScreen;
