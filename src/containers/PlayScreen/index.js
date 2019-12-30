cefea1d51cd188c43c3ae20f4b94f46ea2e84e22
lidiayoshino <lidiayoshino@gmail.com>
2019-12-30 00:48:22 -0200

    refactor(PlayScreen): remove return

--- src/containers/PlayScreen/index.js
+++ src/containers/PlayScreen/index.js
@@ -3,14 +3,12 @@ import React from 'react';
 import Title from '../../components/Title';
 import Header from '../../components/Header';
 
-const PlayScreen = () => {
-  return (
-    <Header>
-      <Title>
-        Play!
-      </Title>
-    </Header>
-  );
-}
+const PlayScreen = () => (
+  <Header>
+    <Title>
+      Play!
+    </Title>
+  </Header>
+);
 
 export default PlayScreen;
