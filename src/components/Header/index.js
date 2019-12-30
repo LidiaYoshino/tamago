12334fcaa07a9fb30ed322a0d099bb5a922cffc4
lidiayoshino <lidiayoshino@gmail.com>
2019-12-29 23:55:18 -0200

    feat(src/index): add themeProvider

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = styled.section`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	font-size: calc(10px + 2vmin);
+	font-size: ${props => props.theme.fontSize.header};
 	color: white;
 `;
 
---
+++ src/styles/Theme.jsx
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        main: '#FFF',
+        backGround: '#282C34',
+    },
+    fontSize: {
+        header: 'calc(10px + 2vmin)',
+    }
+}
